4cf7cf766bfe53a800f23cfa4f7092a9951f60d1
Jeroen Luchtenberg <j.luchtenberg@protonmail.com>
2026-02-03 16:16:30 +0100

    Docker: Enable socket instead of service

--- build_files/build.sh
+++ build_files/build.sh
@@ -13,7 +13,7 @@ set -ouex pipefail
 # Install Docker
 dnf config-manager addrepo --from-repofile https://download.docker.com/linux/fedora/docker-ce.repo
 dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-systemctl enable docker
+systemctl enable docker.socket
 
 # Add vscode repo
 tee /etc/yum.repos.d/vscode.repo <<'EOF'
