a18976a0a347b920d79b217192f417d78ce656c3
Jeroen Luchtenberg <j.luchtenberg@protonmail.com>
2026-02-13 18:41:00 +0100

    Add wipe util

--- build_files/build.sh
+++ build_files/build.sh
@@ -27,7 +27,7 @@ EOF
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 
 # this installs a package from fedora repos
-dnf5 install -y code chezmoi
+dnf5 install -y code chezmoi wipe
 
 # Use a COPR Example:
 #
