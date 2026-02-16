633a80de3793d39e6184ff3112f7e4a665145b26
Klapkop <92648779+Klapkop@users.noreply.github.com>
2026-02-16 20:25:46 +0100

    Build: Add Butane

--- build_files/build.sh
+++ build_files/build.sh
@@ -30,7 +30,7 @@ EOF
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 
 # this installs a package from fedora repos
-dnf5 install -y code chezmoi wipe
+dnf5 install -y code chezmoi wipe butane
 
 # Use a COPR Example:
 #
