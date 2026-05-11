7977f711756d13b4058947ea517dbe3a4ad8c107
Klapkop <92648779+Klapkop@users.noreply.github.com>
2026-05-11 15:52:49 +0200

    Update build.sh to install only openssh-server
    Removed 'virt-install' from the installation command.

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # Setup base
-dnf5 install -y virt-install openssh-server
+dnf5 install -y openssh-server
 
 
 # Install Docker
