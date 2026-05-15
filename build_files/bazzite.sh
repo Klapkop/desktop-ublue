3a9e5105811950553af60f2599cce8b53d4ce8ac
Klapkop <j.luchtenberg@protonmail.com>
2026-05-15 14:18:59 +0200

    Create script for Bazzite specific build steps

---
+++ build_files/bazzite.sh
@@ -0,0 +1,7 @@
+#### Install Flatpacks ####
+cat <<EOF >> /usr/share/ublue-os/bazzite/flatpak/install
+com.brave.Browser
+md.obsidian.Obsidian
+com.spotify.Client
+EOF
+
--- build_files/build.sh
+++ build_files/build.sh
@@ -53,12 +53,9 @@ dnf5 install -y \
 # dnf5 -y copr disable ublue-os/staging
 #
 
-#### Install Flatpacks ####
-cat <<EOF >> /usr/share/ublue-os/bazzite/flatpak/install
-com.brave.Browser
-md.obsidian.Obsidian
-com.spotify.Client
-EOF
+if [[ $BASE_IMAGE = *"bazzite"* ]]; then
+	source bazzite.sh
+fi
 
 #### Example for enabling a System Unit File
 
